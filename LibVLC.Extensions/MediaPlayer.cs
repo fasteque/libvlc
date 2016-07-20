e4cf6c9569ece75a7caf22484eeefe482c84a2a8
Stefan Reinhard <stefan@monostream.com>
2016-07-20 16:33:53 +0200

    New project structure

---
+++ LibVLC.Extensions/MediaPlayer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace VideoLAN
+{
+    public class MediaPlayer : InternalMediaPlayer
+    {
+        public MediaPlayer(IntPtr javaReference, Android.Runtime.JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public MediaPlayer(LibVLC libVlc) : base(libVlc)
+        {
+        }
+
+        public MediaPlayer(VLCMedia media) : base(media)
+        {
+        }
+
+        public event EventHandler MediaChanged;
+
+        public event EventHandler Opening;
+
+        public event EventHandler Buffering;
+
+        public event EventHandler Playing;
+
+        public event EventHandler Paused;
+
+        public event EventHandler Stopped;
+
+        public event EventHandler EndReached;
+
+        public event EventHandler EncouteredError;
+
+        public event EventHandler TimeChanged;
+
+        public event EventHandler PositionChanged;
+
+        public event EventHandler SeekableChanged;
+
+        public event EventHandler PausableChanged;
+
+        public event EventHandler ESAdded;
+
+        public event EventHandler ESDeleted;
+
+        public event EventHandler Vout;
+
+
+        protected override Event OnEventNative(int eventType, long p1, float p2)
+        {
+            switch (eventType)
+            {
+                case Event.MediaChanged:
+                    MediaChanged?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.Opening:
+                    Opening?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.Buffering:
+                    Buffering?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.Playing:
+                    Playing?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.Paused:
+                    Paused?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.Stopped:
+                    Stopped?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.EndReached:
+                    EndReached?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.EncounteredError:
+                    EncouteredError?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.TimeChanged:
+                    TimeChanged?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.PositionChanged:
+                    PositionChanged?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.SeekableChanged:
+                    SeekableChanged?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.PausableChanged:
+                    PausableChanged?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.ESAdded:
+                    ESAdded?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.ESDeleted:
+                    ESDeleted?.Invoke(this, EventArgs.Empty);
+                    break;
+                case Event.Vout:
+                    Vout?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+
+            return base.OnEventNative(eventType, p1, p2);
+        }
+    }
+}
---
+++ Sample/MainActivity.cs
@@ -0,0 +1,158 @@
+﻿using Android.App;
+using Android.OS;
+using VideoLAN;
+using Android.Views;
+using Android.Util;
+using System;
+
+namespace Sample
+{
+    [Activity(Label = "libvlcTest", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        const string TAG = "libVLCTest";
+
+        LibVLC libVLC;
+        MediaPlayer mediaPlayer;
+        SurfaceView surfaceView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            surfaceView = FindViewById<SurfaceView>(Resource.Id.surfaceView);
+
+            if (libVLC == null)
+            {
+                libVLC = new LibVLC();
+                mediaPlayer = new MediaPlayer(libVLC);
+                setMediaPlayerEvents();
+            }
+
+            IVLCVout vout = mediaPlayer.VLCVout;
+            vout.SetVideoView(surfaceView);
+            vout.AttachViews();
+
+            var m = new VLCMedia(libVLC, Android.Net.Uri.Parse("https://www.monobox.ch/assets/Honey_Bees_24fps_4K.mp4"));
+            mediaPlayer.Media = m;
+            mediaPlayer.Rate = 1.0f;
+            mediaPlayer.Play();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            releaseMediaPlayer();
+        }
+
+        private void setMediaPlayerEvents()
+        {
+            mediaPlayer.MediaChanged += OnMediaPlayerMediaChanged;
+            mediaPlayer.Opening += OnMediaPlayerOpening;
+            mediaPlayer.Buffering += OnMediaPlayerBuffering;
+            mediaPlayer.Playing += OnMediaPlayerPlaying;
+            mediaPlayer.Paused += OnMediaPlayerPaused;
+            mediaPlayer.Stopped += OnMediaPlayerStopped;
+            mediaPlayer.EndReached += OnMediaPlayerEndReached;
+            mediaPlayer.EncouteredError += OnMediaPlayerEncouteredError;
+            mediaPlayer.TimeChanged += OnMediaPlayerTimeChanged;
+            mediaPlayer.PositionChanged += OnMediaPlayerPositionChanged;
+            mediaPlayer.SeekableChanged += OnMediaPlayerSeekableChanged;
+            mediaPlayer.PausableChanged += OnMediaPlayerPausableChanged;
+            mediaPlayer.ESAdded += OnMediaPlayerESAdded;
+            mediaPlayer.ESDeleted += OnMediaPlayerESDeleted;
+            mediaPlayer.Vout += OnMediaPlayerVout;
+        }
+
+        void releaseMediaPlayer()
+        {
+            if (libVLC == null)
+                return;
+
+            if (mediaPlayer != null)
+            {
+                mediaPlayer.Stop();
+                IVLCVout vout = mediaPlayer.VLCVout;
+                vout.DetachViews();
+                libVLC.Release();
+                libVLC = null;
+            }
+        }
+
+        private void OnMediaPlayerMediaChanged(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: MediaChanged");
+        }
+
+        private void OnMediaPlayerOpening(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: Opening");
+        }
+
+        private void OnMediaPlayerBuffering(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: Buffering");
+        }
+
+        private void OnMediaPlayerPlaying(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: Playing");
+        }
+
+        private void OnMediaPlayerPaused(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: Paused");
+        }
+
+        private void OnMediaPlayerStopped(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: Stopped");
+        }
+
+        private void OnMediaPlayerEndReached(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: EndReached");
+        }
+
+        private void OnMediaPlayerEncouteredError(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: EncounteredError");
+        }
+
+        private void OnMediaPlayerTimeChanged(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: TimeChanged");
+        }
+
+        private void OnMediaPlayerPositionChanged(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: PositionChanged");
+        }
+
+        private void OnMediaPlayerSeekableChanged(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: SeekableChanged");
+        }
+
+        private void OnMediaPlayerPausableChanged(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: PausableChanged");
+        }
+
+        private void OnMediaPlayerESAdded(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: ESAdded");
+        }
+
+        private void OnMediaPlayerESDeleted(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: ESDeleted");
+        }
+
+        private void OnMediaPlayerVout(object sender, EventArgs args)
+        {
+            Log.Debug(TAG, "Media player: Vout");
+        }
+    }
+}
